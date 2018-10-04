9ba0e9b899d6bc22b5245af32d36b65b9d375bbb
Pablo Araya <paraya@mobdev.cl>
2018-10-04 18:43:12 -0300

    Added Recyclerview

--- app/src/main/java/pabloaraya/org/moviesparty/activity/MainActivity.java
+++ app/src/main/java/pabloaraya/org/moviesparty/activity/MainActivity.java
@@ -3,6 +3,8 @@ package pabloaraya.org.moviesparty.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
@@ -10,11 +12,16 @@ import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
 import pabloaraya.org.moviesparty.R;
+import pabloaraya.org.moviesparty.adapter.MovieAdapter;
 import pabloaraya.org.view.contract.MovieContract;
 import pabloaraya.org.view.modelview.MovieModelView;
 import pabloaraya.org.view.presenter.MoviePresenter;
 
 public class MainActivity extends AppCompatActivity implements MovieContract.View {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Inject
     MoviePresenter presenter;
@@ -25,13 +32,21 @@ public class MainActivity extends AppCompatActivity implements MovieContract.Vie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         presenter.attachView(this);
         presenter.loadMovies();
     }
 
     @Override
     public void setMovieItems(List<MovieModelView> movieItems) {
-
+        mAdapter = new MovieAdapter(movieItems);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
---
+++ app/src/main/java/pabloaraya/org/moviesparty/adapter/MovieAdapter.java
@@ -0,0 +1,61 @@
+package pabloaraya.org.moviesparty.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pabloaraya.org.moviesparty.R;
+import pabloaraya.org.view.modelview.MovieModelView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+
+    private List<MovieModelView> movieModelViews;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            mTextView = v.findViewById(R.id.my_text_view);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MovieAdapter(List<MovieModelView> movieModelViews) {
+        this.movieModelViews = movieModelViews;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MovieAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(movieModelViews.get(position).title);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return movieModelViews.size();
+    }
+}
